c0b7275b7116f0f864a1ec57bae1742f6476dad4
Chris Ritter <thechrisritter@gmail.com>
2014-09-17 13:17:07 -0500

    Update coin_flip2.py

--- Solutions/coin_flip2.py
+++ Solutions/coin_flip2.py
@@ -3,11 +3,11 @@
 
 import random
 
-samples = [ random.randint(0, 1) for i in range(100) ]
-heads = samples.count(0)
-tails = samples.count(1)
-for s in samples:
+flips = [ random.randint(0, 1) for i in range(100) ]
+heads = flips.count(0)
+tails = flips.count(1)
+for s in flips:
 	msg = "Heads" if s==1 else "Tails"
 	print(msg)
 print("Heads count=%d, Tails count=%d" % (heads, tails))
-input("\n\nPress the enter key to exit.")
+input()
